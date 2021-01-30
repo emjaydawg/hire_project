b76871e063fe539c04849b274e866ae679a5e71d
Glen Jarvis <glen@glenjarvis.com>
2021-01-29 21:20:12 -0800

    Interrim step

--- hire/hireapp/admin.py
+++ hire/hireapp/admin.py
@@ -1,10 +1,38 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-from .models import Employee
-from .models import Employer
-from .models import Job
+from .models import Company, Employee, HiringManager, Job, Profile, Rating
 
-# Register your models here.
-admin.site.register(Employee)
-admin.site.register(Employer)
-admin.site.register(Job)
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = "Profile"
+    fk_name = "user"
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline, )
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
+
+admin.site.register(
+    Company,
+    readonly_fields=('slug',))
+
+admin.site.register(HiringManager)
+
+admin.site.register(Employee,)
+
+admin.site.register(
+    Job,
+    readonly_fields=('slug',))
+
+#admin.site.register(Message,)
+admin.site.register(Profile,)
+admin.site.register(Rating,)
--- hire/hireapp/models.py
+++ hire/hireapp/models.py
@@ -1,28 +1,152 @@
+
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.utils.text import slugify
+
+
+class Profile(models.Model):
+    STAFF = 1
+    COMPANY_CONTACT = 2
+    EMPLOYEE = 3
+    UNDEFINED_PROFILE = 4
+    PROFILE_CHOICES = (
+        (STAFF, "Staff"),
+        (COMPANY_CONTACT, "Company Contact"),
+        (EMPLOYEE, "Future Employee"),
+        (UNDEFINED_PROFILE, "Non-Classified Profile"),
+    )
+    user = models.OneToOneField(
+        User,
+        on_delete=models.RESTRICT)
+    phone_number = models.CharField(
+        max_length=12,
+        blank=True,
+        null=True,
+        unique=True
+    )
+    profile_type = models.PositiveSmallIntegerField(
+        choices=PROFILE_CHOICES,
+        null=True,
+        blank=True)
+
+    def __str__(self):
+        roles = dict(self.PROFILE_CHOICES)
+        if self.profile_type:
+            label = roles[self.profile_type]
+        else:
+            label = "Orphaned Profile"
+        return "%s: %s" % (label, self.user.username)
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    try:
+        instance.profile.save()
+    except User.profile.RelatedObjectDoesNotExist:
+        Profile(user=instance).save()
+
 
 class Employee(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    # Intentionally leaving this a text field
+    # until we better understand hiring needs
+    # HERE
+    user = models.OneToOneField(
+        User,
+        on_delete=models.RESTRICT)
+    skills = models.TextField(
+        blank=True,
+        null=True)
+    ratings = models.ManyToManyField(
+        'Rating')
+
+class HiringManager(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.RESTRICT)
+    company = models.ForeignKey(
+        'Company',
+        blank=True,
+        null=True,
+        on_delete=models.RESTRICT)
 
     def __str__(self):
-        return "%s %s" % (self.first_name, self.last_name)
+        user = self.user
+        name = ""
+        if user.first_name or user.last_name:
+            name = "%s %s" % (user.first_name, user.last_name)
+        else:
+            name = user.username
+        return "%s: %s" % (self.company.name, name)
 
-class Employer(models.Model):
-    # Should we call this company? Many other sites do
-    company_name = models.CharField(max_length=30)
-    company_description = models.TextField()
-    website = models.URLField(max_length=200)
-    company_name = models.CharField(max_length=30)
-    company_size = models.CharField(max_length=30)
-    founded = models.IntegerField()
+class Rating(models.Model):
+    score = models.PositiveSmallIntegerField(
+        validators=[MaxValueValidator(10), MinValueValidator(1)]
+    )
+
+
+class Company(models.Model):
+    """Business Entity that is hiring"""
+
+    MAX_NAME_LENGTH = 85
+
+    slug = models.SlugField(
+        unique=True,
+        default='',
+        editable=False,
+        max_length=MAX_NAME_LENGTH)
+
+    name = models.CharField(
+        max_length=MAX_NAME_LENGTH
+    )
+    description = models.TextField(
+        blank=True,
+        null=True)
+    street = models.CharField(
+        max_length=60,
+        blank=True,
+        null=True)
+    city = models.CharField(
+        max_length=17,
+        blank=True,
+        null=True)
+    state = models.CharField(
+        max_length=2,
+        blank=True,
+        null=True)
+    postal_code = models.CharField(
+        max_length=10,
+        blank=True,
+        null=True)
+    website = models.URLField(
+        max_length=200,
+        blank=True,
+        null=True)
+
+    def save(self, *args, **kwargs):
+        value = self.name
+        if not self.slug:
+            self.slug = slugify(value, allow_unicode=True)
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return self.company_name
+        return "%s (%s)" % (self.slug, self.name)
+
+
 
 class Job(models.Model):
+    MAX_TITLE_LENGTH = 30
     NOT_SPECIFIED = "N"
     FULL_TIME = "F"
     PART_TIME = "P"
+
+    RATE_HOUR = "H"
+    RATE_WEEK = "W"
+    RATE_MONTH = "M"
+    RATE_YEAR = "Y"
 
     WORK_TYPE_CHOICES = [
       (NOT_SPECIFIED, 'Not Specified'),
@@ -30,18 +154,66 @@ class Job(models.Model):
       (PART_TIME, 'Part Time'),
     ]
 
-    name = models.CharField(max_length=30)
-    location = models.CharField(max_length=30)
+    RATE_TYPE_CHOICES = [
+      (NOT_SPECIFIED, 'Not Specified'),
+      (RATE_HOUR, 'Hourly'),
+      (RATE_WEEK, 'Weekly'),
+      (RATE_MONTH, 'Monthly'),
+      (RATE_YEAR, 'Yearly'),
+    ]
+
+    slug = models.SlugField(
+        unique=True,
+        default='',
+        editable=False,
+        max_length=Company.MAX_NAME_LENGTH + MAX_TITLE_LENGTH + 1)
+
+    title = models.CharField(
+        max_length=MAX_TITLE_LENGTH)
+    location = models.CharField(
+        max_length=30)
     summary = models.TextField()
     requirements = models.TextField()
-    WorkType = models.CharField(
+    work_type = models.CharField(
         max_length=1,
         choices=WORK_TYPE_CHOICES,
-        default=NOT_SPECIFIED
-    )
-    # When posted
-    # Industries
+        default=NOT_SPECIFIED)
+    posted_date = models.DateTimeField(
+        auto_now_add=True)
+    company = models.ForeignKey(
+        Company,
+        on_delete=models.RESTRICT)
+    rate_amount = models.DecimalField(
+        decimal_places=2,
+        max_digits=8,
+        blank=True,
+        null=True)
+    rate_unit = models.CharField(
+        max_length=1,
+        choices=RATE_TYPE_CHOICES,
+        default=NOT_SPECIFIED)
+
+    # Applications
+
+    def save(self, *args, **kwargs):
+        value = self.title
+        if not self.slug:
+            self.slug = slugify(
+                "%s-%s" % (self.company.slug, value),
+                allow_unicode=True)
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return self.name
+        return "%s (%s)" % (self.title, self.slug)
 
+class Applications(models.Model):
+    # job
+    # status
+    # start_date
+    pass
+
+class Ratings(models.Model):
+    pass
+
+class Messages(models.Model):
+    pass
